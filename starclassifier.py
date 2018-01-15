4314f7567f2ae39147738d61908614149907cb4c
Pablo Duque <menduke@live.com>
2018-01-16 00:30:21 +0100

    Fixed batch feeding

--- starclassifier.py
+++ starclassifier.py
@@ -14,7 +14,7 @@ class StarClassifier():
 
         # Parameters
         learning_rate = 0.0001
-        training_epochs = 6000
+        training_epochs = 2000
         batch_size = 10
         display_step = 100
 
@@ -22,7 +22,7 @@ class StarClassifier():
         n_input = 100 # 
         n_classes = 2 #
 
-        n_hidden_1 = 51 # 1st layer number of neurons
+        n_hidden_1 = 2 # 1st layer number of neurons
          
         # tf Graph input
         X = tf.placeholder("float", [None, n_input])
@@ -64,14 +64,19 @@ class StarClassifier():
                 avg_cost = 0.
                 total_batch = len(self.training_data)//batch_size
                 # Loop over all batches
-                for i in range(total_batch):
+                i = 0
+                while i < len(self.training_data):
                     batch_x = self.training_data[i:i+batch_size]
                     batch_y = self.training_labels[i:i+batch_size]
+                    #print("Batch: ", self.training_labels[i:i+batch_size] == self.training_labels[i+batch_size:i+(batch_size*2)])
                     # Run optimization op (backprop) and cost op (to get loss value)
                     _, cost = sess.run([train_op, error], feed_dict={X: batch_x, Y: batch_y})
                     ordered_costs.append(cost)
                     # Compute average loss
                     avg_cost += cost / total_batch
+
+                    i += batch_size
+
                 # Display logs per epoch step
                 if epoch % display_step == 0:
                     print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_cost))
